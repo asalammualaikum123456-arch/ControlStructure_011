548351f877de446207c4f88959aa5af36085f6c3
asalammualaikum123456-arch <asalammualaikum123456@gmail.com>
2026-09-24 11:03:08 +0700

    first commit

---
+++ tugas.py
@@ -0,0 +1,11 @@
+persen = float(input("Masukkan persentase: "))
+if persen >= 90:
+    print("the Excellent performance")
+elif persen >= 80:
+    print("then Very Good performance")
+elif persen >= 70:
+    print("then Good performance")
+elif persen >= 60:
+    print("then Average performance")
+else:
+    print("then Poor performance")
